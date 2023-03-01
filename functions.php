fd9b69cb5dcd2f680e65f741ba87718992533bba
Kateryna Yakovleva <katyayakovleva2905@gmail.com>
2023-03-01 11:22:10 +0200

    edit some files

--- functions.php
+++ functions.php
@@ -50,6 +50,9 @@ function sibosfurniture_setup() {
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'sibosfurniture' ),
+			'footer-menu' => esc_html__( 'Footer menu', 'sibosfurniture' ),
+			'footer-links' => esc_html__( 'Footer links', 'sibosfurniture' ),
+			'home-page-menu' => esc_html__( 'Home Page Menu', 'sibosfurniture' ),
 		)
 	);
 
